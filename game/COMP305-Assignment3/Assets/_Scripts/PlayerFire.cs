b11e2466d72fb9ccf6b92b82835c3f440f1591ef
Aaron Fernandes <aagavin@gmail.com>
2016-11-19 18:29:16 -0500

    new fire

---
+++ game/COMP305-Assignment3/Assets/_Scripts/PlayerFire.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFire : MonoBehaviour {
+
+
+	//public
+	public AudioSource Firesound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Firesound.Play ();
+		}
+	}
+}
